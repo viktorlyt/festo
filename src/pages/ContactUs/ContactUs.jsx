a56407c71f4909a4968711bb2220f3d50421f802
viktorlyt <viktorlyt@gmail.com>
2022-09-27 19:27:15 +0300

    contactUs added

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -5,64 +5,122 @@ import { Subscribe } from '../../components/Subscribe'
 
 export const ContactUs = () => {
   return (
-    <div className='ContactUs'>
+    <div className='contactUs'>
       <Header />
-      <div className='home__banner' style={{ backgroundImage: "url(/contact-us_banner.png)" }}>
-        <div className='home__banner-center'>
-          <span className='home__span1'>
-            Look at the map and discover house parties!
-          </span>
-          <span className='home__span2'>
-            Host a party yourself and make money on party tickets!
-          </span>
-          <div className='home__span-reg'>
-            <span className='home__span3'>
-              Register your 
-            </span>
-            <span className='home__span4'>
-              INTEREST
-            </span>
-            <span className='home__span5'>
-              now!
-            </span>
-          </div>
-          <span className='home__mobiles'>
-            <img 
-              src="Festo-party-app.png" 
-              alt="Festo-party-app"
-              className='home__mobile1'
-            />
-            <img 
-              src="festo-app-on-mobile.png" 
-              alt="festo-app-on-mobile"
-              className='home__mobile2' 
-            />
-          </span>
+      <div className='contactUs__banner'></div>
+
+      <div className='contactUs__gray'></div>
+      <div className='contactUs__white'></div>
+
+      <section className='festo-easy contactUs__register'>
+        <div className='festo-easy__red contactUs__register--red'>
+          <img 
+            src="logo-big.png" 
+            alt="logo-big" 
+            className='contactUs__register--red-logo'
+          />
         </div>
-        <img 
-          src="red_shadow.png" 
-          alt="red_shadow"
-          className='home__red_shadow'
-        />
-      </div>
+        <div className='contactUs__register--infobox'>
+          <div className='contactUs__register--info'>
+            <h3 className='contactUs__register--info-h2'>Register your interest.</h3>
 
-      <div className='home__redSection'>
-        <span className='home__redSection-span1'>
-          What is Festo?
-        </span>
-        <span className='home__redSection-span2'>
-          Meet Festo the all-in-one solution <br/> for efficient party planning
-        </span>
-        <span className='home__redSection-span3'>
-          Register your interest today and learn about next steps to set-up your party
-        </span>
-      </div>
+            <form 
+              action="" 
+              method='post'
+              className='contactUs__register--form'
+            >
+              <div className='contactUs__register--form-row'>
+                <span className='contactUs__register--form-row__item'>
+                  <label htmlFor="firstName" className='contactUs__register--form-row__item--label'>
+                    First Name
+                  </label>
+                  <input
+                    className='contactUs__register--form-row__item--input'
+                    type='text'
+                    id='firstName'
+                    required
+                  >
+                  </input>
+                </span>
+                <span className='contactUs__register--form-row__item'>
+                  <label htmlFor="lastName" className='contactUs__register--form-row__item--label-NR'>
+                    Last Name
+                  </label>
+                  <input
+                    className='contactUs__register--form-row__item--input'
+                    type='text'
+                    id='lastName'
+                  >
+                  </input>
+                </span>
+              </div>
+              
+              <div className='contactUs__register--form-row'>
+                <span className='contactUs__register--form-row__item'>
+                  <label htmlFor="email" className='contactUs__register--form-row__item--label'>
+                    Email
+                  </label>
+                  <input
+                    className='contactUs__register--form-row__item--input'
+                    type='email'
+                    id='email'
+                    required
+                    pattern="^.+@.+\.[a-zA-Z]{2,63}$"
+                  >
+                  </input>
+                </span>
+                <span className='contactUs__register--form-row__item'>
+                  <label htmlFor="phone" className='contactUs__register--form-row__item--label-NR'>
+                    Phone
+                  </label>
+                  <input
+                    className='contactUs__register--form-row__item--input'
+                    type='phone'
+                    id='phone'
+                  >
+                  </input>
+                </span>
+              </div>
+              
+              <div className='contactUs__register--form-row'>
+                <span className='contactUs__register--form-row__item'>
+                  <label htmlFor="massage" className='contactUs__register--form-row__item--label-NR'>
+                    Leave us a message...
+                  </label>
+                  <textarea
+                    className='contactUs__register--form-row__item--textarea'
+                    type='text'
+                    id='massage'
+                  >
+                  </textarea>
+                </span>
+              </div>
 
-      <div className='home__gray'></div>
+              <div className='contactUs__register--form-row'>
+                <span className='contactUs__register--form-row__item'>
+                  <label htmlFor="company" className='contactUs__register--form-row__item--label-NR'>
+                    Company
+                  </label>
+                  <input
+                    className='contactUs__register--form-row__item--input'
+                    type='text'
+                    id='company'
+                    placeholder='company'
+                  >
+                  </input>
+                </span>
+              </div>
 
-      <Subscribe />
+              <button type='submit' className='contactUs__register--form-btn'>
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </section>
 
-      <div className='home__gray'></div>
+      <div className='contactUs__white'></div>
+      <div className='contactUs__red'></div>
 
       <Subscribe />
       <Footer />
