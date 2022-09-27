9c7b44eba2fd42685b55cbe40065d33247b244e4
viktorlyt <viktorlyt@gmail.com>
2022-09-27 16:14:57 +0300

    features apps added

--- src/pages/Features/Features.jsx
+++ src/pages/Features/Features.jsx
@@ -7,11 +7,134 @@ export const Features = () => {
   return (
     <div className='features'>
       <Header />
-      <div 
-        className='features__banner' 
-        style={{ backgroundImage: "url(/features_banner.png)" }}
-      >
+
+      <div className='features__banner'></div>
+      
+      <div className='features__text'>
+        <h1 className='features__text--text1'>Festo App Features</h1>
+        <span className='features__text--text2'>
+          Festo party app is hassle-free and is packed with features to make organising a party simple and easy.
+        </span>
       </div>
+
+      <section className='features__apps'>
+        <div className='features__apps--box'>
+          <img 
+            src="f1.svg" 
+            alt="f1"
+            className='features__apps--box-img' 
+          />
+          <p className='features__apps--box-p'>Create Profile</p>
+        </div>
+        
+        <div className='features__apps--boxrow'>
+          <div className='features__apps--box'>
+            <img
+              src="f2.svg"
+              alt="f2"
+              className='features__apps--box-img'
+            />
+            <p className='features__apps--box-p'>Find Friends</p>
+          </div>
+          <div className='features__apps--box'>
+            <img
+              src="f3.svg"
+              alt="f3"
+              className='features__apps--box-img'
+            />
+            <p className='features__apps--box-p'>Add Friends</p>
+          </div>
+          <div className='features__apps--box'>
+            <img
+              src="f4.svg"
+              alt="f4"
+              className='features__apps--box-img'
+            />
+            <p className='features__apps--box-p'>Block People</p>
+          </div>
+        </div>
+        
+        <div className='features__apps--boxrow'>
+          <div className='features__apps--box'>
+            <img 
+              src="f5.svg" 
+              alt="f5"
+              className='features__apps--box-img' 
+            />
+            <p className='features__apps--box-p'>Create a Partiy</p>
+          </div>
+          <div className='features__apps--box'>
+            <img 
+              src="f6.svg" 
+              alt="f6"
+              className='features__apps--box-img' 
+            />
+            <p className='features__apps--box-p'>Public Parties</p>
+          </div>
+          <div className='features__apps--box'>
+            <img 
+              src="f7.svg" 
+              alt="f7"
+              className='features__apps--box-img' 
+            />
+            <p className='features__apps--box-p'>Private Parties</p>
+          </div>
+        </div>
+
+        <div className='features__apps--boxrow'>
+          <div className='features__apps--box'>
+            <img 
+              src="f8.svg" 
+              alt="f8"
+              className='features__apps--box-img' 
+            />
+            <p className='features__apps--box-p'>Discover Parties</p>
+          </div>
+          <div className='features__apps--box'>
+            <img 
+              src="f9.svg" 
+              alt="f9"
+              className='features__apps--box-img' 
+            />
+            <p className='features__apps--box-p'>Add Bank</p>
+          </div>
+          <div className='features__apps--box'>
+            <img 
+              src="f10.svg" 
+              alt="f10"
+              className='features__apps--box-img' 
+            />
+            <p className='features__apps--box-p'>Secure Payments</p>
+          </div>
+        </div>
+        
+        <div className='features__apps--boxrow'>
+          <div className='features__apps--box'>
+            <img 
+              src="f11.svg" 
+              alt="f11"
+              className='features__apps--box-img' 
+            />
+            <p className='features__apps--box-p'>Chat on App</p>
+          </div>
+          <div className='features__apps--box'>
+            <img 
+              src="f12.svg" 
+              alt="f12"
+              className='features__apps--box-img' 
+            />
+            <p className='features__apps--box-p'>Notifications</p>
+          </div>
+          <div className='features__apps--box'>
+            <img 
+              src="f13.png" 
+              alt="f13"
+              className='features__apps--box-img' 
+            />
+            <p className='features__apps--box-p'>Scan QR codes</p>
+          </div>
+        </div>
+      </section>
 
       <Subscribe />
       <Footer />
